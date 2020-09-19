4758b5798bdbaa05682db6bdf23d7613b934ec27
Marzanos <punthita.marz@mail.kmutt.ac.th>
2020-09-19 20:57:44 +0700

    Create_unity_project

---
+++ Unity/Game_NSC/Assets/Script/WalkingPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingPlayer : MonoBehaviour
+{
+    public float SpeedMovement = 3.0f;
+    public Camera cameramain;
+    private Rigidbody RB;
+    public float Speed=0;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * SpeedMovement;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * SpeedMovement;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * SpeedMovement;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * SpeedMovement;
+        }
+        if (Input.GetKey(KeyCode.Space) && transform.position.y<=1)
+        {
+            RB.AddForce(Vector3.up * Speed);
+            if (transform.position.y >= 1)
+            {
+                Speed = 0;
+            }
+            else
+            {
+                Speed = 20;
+            }
+            
+        }
+        Quaternion rotate = Quaternion.Euler(0, cameramain.transform.rotation.eulerAngles.y, 0);
+        transform.rotation = rotate;
+
+    }
+
+}
